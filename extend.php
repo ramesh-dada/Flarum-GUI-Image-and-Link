257e8b6e26f041e154dd5b7ca6fa8fe1c042d7b8
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-08 15:34:00 +0530

    Update extend.php

--- extend.php
+++ extend.php
@@ -10,7 +10,7 @@ return [
     (new Extend\Frontend('forum'))
     ->content(function (Document $document) {
         $document->head[] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.0.18/sweetalert2.all.min.js"></script>';
-        $document->head[] = '<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/ramesh-dada/Flarum-GUI-Image-and-Link/assets/image-and-link-gui.js"></script>';
+        $document->head[] = '<script type="text/javascript" src="https://cdn.statically.io/gh/ramesh-dada/Flarum-GUI-Image-and-Link/main/assets/image-and-link-gui.js"></script>';
     })
         ->js(__DIR__ . '/js/dist/forum.js')
         ->css(__DIR__ . '/resources/less/forum.less'),
